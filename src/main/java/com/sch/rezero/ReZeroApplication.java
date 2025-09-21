cb477785396b6ffb72c3e20c7a3b7b53243e4611
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-09-21 20:46:28 +0900

    first commit

---
+++ src/main/java/com/sch/rezero/ReZeroApplication.java
@@ -0,0 +1,13 @@
+package com.sch.rezero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReZeroApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReZeroApplication.class, args);
+  }
+
+}
