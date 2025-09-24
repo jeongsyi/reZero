0bdcddeec2eba46ea8da4f8be4484c01e4f9ea93
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-09-24 17:29:24 +0900

    feat : CommunityPost Entity 추가

---
+++ src/main/java/com/sch/rezero/entity/community/CommunityPost.java
@@ -0,0 +1,40 @@
+package com.sch.rezero.entity.community;
+
+import com.sch.rezero.entity.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "community_posts")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class CommunityPost {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  User user;
+
+  @Column(nullable = false)
+  String title;
+
+  @Column(nullable = false)
+  String description;
+
+
+}
