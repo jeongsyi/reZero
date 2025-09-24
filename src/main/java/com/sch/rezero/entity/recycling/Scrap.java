d5aa33d4293473f394152623eead1789aa4b5b93
j2eun0922 <j2eun0922@naver.com>
2025-09-24 18:23:46 +0900

    feat : scrap 엔티티 추가

---
+++ src/main/java/com/sch/rezero/entity/recycling/Scrap.java
@@ -0,0 +1,32 @@
+package com.sch.rezero.entity.recycling;
+
+import com.sch.rezero.entity.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "scraps")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Scrap {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "recycling_id", nullable = false)
+    private RecyclingPost post;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
