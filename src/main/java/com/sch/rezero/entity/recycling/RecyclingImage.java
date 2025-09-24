49ee34586c0286edc236224f9a27ba2a7c3a2687
j2eun0922 <j2eun0922@naver.com>
2025-09-24 18:32:34 +0900

    feat : recyclingImage 엔티티 추가

--- src/main/java/com/sch/rezero/entity/recycling/RecyclingImage.java
+++ src/main/java/com/sch/rezero/entity/recycling/RecyclingImage.java
@@ -1,21 +1,21 @@
 package com.sch.rezero.entity.recycling;
 
-import com.sch.rezero.entity.user.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Table(name = "scraps")
+@Table(name = "recycling_images")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Scrap {
+public class RecyclingImage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,11 +24,14 @@ public class Scrap {
     @JoinColumn(name = "recycling_id", nullable = false)
     private RecyclingPost post;
 
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
 
     @CreationTimestamp
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at", updatable = false, nullable = false)
     private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
