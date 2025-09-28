2705ede18c3b0894903f0f95f04910da9fa470d2
j2eun0922 <j2eun0922@naver.com>
2025-09-28 15:49:05 +0900

    feat : userRepository 추가

---
+++ src/main/java/com/sch/rezero/repository/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sch.rezero.repository.user;
+
+import com.sch.rezero.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
