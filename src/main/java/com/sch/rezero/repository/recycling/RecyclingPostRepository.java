8d7d9315f90135f7796759c473cb0559e884e327
j2eun0922 <j2eun0922@naver.com>
2025-09-28 17:36:34 +0900

    feat : findAllByUserId 메서드 추가

--- src/main/java/com/sch/rezero/repository/recycling/RecyclingPostRepository.java
+++ src/main/java/com/sch/rezero/repository/recycling/RecyclingPostRepository.java
@@ -4,6 +4,9 @@ import com.sch.rezero.entity.recycling.RecyclingPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecyclingPostRepository extends JpaRepository<RecyclingPost, Long> {
+    List<RecyclingPost> findAllByUserId(Long userId);
 }
