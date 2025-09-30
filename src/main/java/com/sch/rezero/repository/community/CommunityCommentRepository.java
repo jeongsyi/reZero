fef894f49a9e080c4014446a6559387a41c9c613
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-09-30 14:21:10 +0900

    feat: Repository 어노테이션 추가

--- src/main/java/com/sch/rezero/repository/community/CommunityCommentRepository.java
+++ src/main/java/com/sch/rezero/repository/community/CommunityCommentRepository.java
@@ -2,7 +2,9 @@ package com.sch.rezero.repository.community;
 
 import com.sch.rezero.entity.community.CommunityComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommunityCommentRepository extends JpaRepository<CommunityComment, Long> {
 
 }
--- src/main/java/com/sch/rezero/repository/community/LikeRepository.java
+++ src/main/java/com/sch/rezero/repository/community/LikeRepository.java
@@ -2,7 +2,9 @@ package com.sch.rezero.repository.community;
 
 import com.sch.rezero.entity.community.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
 }
