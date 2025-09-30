96d73e70367e9c2972aa9f8a89ad576cb3a27bb8
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-09-30 18:57:01 +0900

    feat: Like Service 추가

--- src/main/java/com/sch/rezero/repository/community/LikeRepository.java
+++ src/main/java/com/sch/rezero/repository/community/LikeRepository.java
@@ -1,10 +1,13 @@
 package com.sch.rezero.repository.community;
 
+import com.sch.rezero.entity.community.CommunityPost;
 import com.sch.rezero.entity.community.Like;
+import com.sch.rezero.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
-
+  boolean existsByUserAndCommunityPost(User user, CommunityPost communityPost);
+  void deleteByUserAndCommunityPost(User user, CommunityPost communityPost);
 }
