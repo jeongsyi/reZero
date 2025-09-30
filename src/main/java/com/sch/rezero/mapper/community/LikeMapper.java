406e4a9cfb92b68647976d5c42282f1df5fa5b90
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-09-30 18:43:31 +0900

    feat: LikeMapper 추가

--- src/main/java/com/sch/rezero/mapper/community/LikeMapper.java
+++ src/main/java/com/sch/rezero/mapper/community/LikeMapper.java
@@ -2,10 +2,14 @@ package com.sch.rezero.mapper.community;
 
 import com.sch.rezero.dto.community.likes.LikeResponse;
 import com.sch.rezero.entity.community.Like;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+@Mapper(componentModel = "spring")
 public interface LikeMapper {
 
+  @Mapping(source = "communityPost.id", target = "postId")
+  @Mapping(source = "user.id", target = "userId")
   LikeResponse toLikeResponse(Like like);
 
 }
