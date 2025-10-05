4e7b2afaf71cf0b8e6ffb5c4b1233096211dd61f
j2eun0922 <j2eun0922@naver.com>
2025-10-05 14:11:23 +0900

    chore: 사용하지 않는 import 제거

--- src/main/java/com/sch/rezero/service/user/FollowService.java
+++ src/main/java/com/sch/rezero/service/user/FollowService.java
@@ -2,15 +2,12 @@ package com.sch.rezero.service.user;
 
 import com.sch.rezero.dto.user.follow.FollowCreateRequest;
 import com.sch.rezero.dto.user.follow.FollowResponse;
-import com.sch.rezero.dto.user.profile.UserResponse;
 import com.sch.rezero.entity.user.Follow;
 import com.sch.rezero.entity.user.User;
 import com.sch.rezero.mapper.user.FollowMapper;
-import com.sch.rezero.mapper.user.UserMapper;
 import com.sch.rezero.repository.user.FollowRepository;
 import com.sch.rezero.repository.user.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,45 +16,45 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class FollowService {
 
-  private final FollowRepository followRepository;
-  private final UserRepository userRepository;
-  private final FollowMapper followMapper;
+    private final FollowRepository followRepository;
+    private final UserRepository userRepository;
+    private final FollowMapper followMapper;
 
-  @Transactional
-  public FollowResponse create(Long id, FollowCreateRequest followCreateRequest) {
+    @Transactional
+    public FollowResponse create(Long id, FollowCreateRequest followCreateRequest) {
 
-    User follower = userRepository.findById(id)
-        .orElseThrow(() -> new EntityNotFoundException("User not found"));
+        User follower = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
 
-    User following = userRepository.findById(followCreateRequest.followingId())
-        .orElseThrow(() -> new EntityNotFoundException("Following not found"));
+        User following = userRepository.findById(followCreateRequest.followingId())
+                .orElseThrow(() -> new EntityNotFoundException("Following not found"));
 
-    if (follower.equals(following)) {
-      throw new IllegalArgumentException("Follower and following are the same");
+        if (follower.equals(following)) {
+            throw new IllegalArgumentException("Follower and following are the same");
+        }
+
+        if (followRepository.existsByFollowerAndFollowing(follower, following)) {
+            throw new IllegalArgumentException("Follower and following are the same");
+        }
+
+        Follow follow = new Follow(follower, following);
+        followRepository.save(follow);
+
+        return followMapper.toFollowResponse(follow);
     }
 
-    if (followRepository.existsByFollowerAndFollowing(follower, following)) {
-      throw new IllegalArgumentException("Follower and following are the same");
+    public void delete(Long followerId, Long followingId) {
+        User follower = userRepository.findById(followerId)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+        User following = userRepository.findById(followingId)
+                .orElseThrow(() -> new EntityNotFoundException("Following not found"));
+
+        if (!followRepository.existsByFollowerAndFollowing(follower, following)) {
+            throw new IllegalArgumentException("Follower and following are not the same");
+        }
+
+        followRepository.deleteByFollowerAndFollowing(follower, following);
     }
-
-    Follow follow = new Follow(follower, following);
-    followRepository.save(follow);
-
-    return followMapper.toFollowResponse(follow);
-  }
-
-  public void delete(Long followerId, Long followingId) {
-    User follower = userRepository.findById(followerId)
-        .orElseThrow(() -> new EntityNotFoundException("User not found"));
-
-    User following = userRepository.findById(followingId)
-        .orElseThrow(() -> new EntityNotFoundException("Following not found"));
-
-    if (!followRepository.existsByFollowerAndFollowing(follower, following)) {
-      throw new IllegalArgumentException("Follower and following are not the same");
-    }
-
-    followRepository.deleteByFollowerAndFollowing(follower, following);
-  }
 
 }
