e2245ab99ff739e0bbfb413621b67ce38947dcf3
j2eun0922 <j2eun0922@naver.com>
2025-10-14 10:02:33 +0900

    feat: JpaConfig 클래스 추가

---
+++ src/main/java/com/sch/rezero/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.sch.rezero.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableJpaAuditing
+@EnableScheduling
+public class JpaConfig {
+}
