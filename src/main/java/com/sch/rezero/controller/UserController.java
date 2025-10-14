118e1005f606661a3b18a5ff75717a3e769c1838
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-10-14 09:23:27 +0900

    fix : UserController로 이름 변경

--- src/main/java/com/sch/rezero/controller/UserController.java
+++ src/main/java/com/sch/rezero/controller/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/me")
-public class ProfileController {
+public class UserController {
 
   private final ProfileService profileService;
   private final UserService userService;
