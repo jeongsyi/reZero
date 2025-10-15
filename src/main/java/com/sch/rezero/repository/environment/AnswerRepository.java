b0dd824aede3f55d3fe2ef9691882a8519590e68
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-10-15 10:26:40 +0900

    feat: 환경 레벨 테스트 관련 Repository 생성

---
+++ src/main/java/com/sch/rezero/repository/environment/AnswerRepository.java
@@ -0,0 +1,8 @@
+package com.sch.rezero.repository.environment;
+
+import com.sch.rezero.entity.environment.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+}
---
+++ src/main/java/com/sch/rezero/repository/environment/LevelRepository.java
@@ -0,0 +1,8 @@
+package com.sch.rezero.repository.environment;
+
+import com.sch.rezero.entity.environment.Level;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LevelRepository extends JpaRepository<Level, Long> {
+
+}
