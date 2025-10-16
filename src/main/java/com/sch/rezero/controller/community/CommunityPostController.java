2b73ea94e688d8121cec54582909f57caa20f318
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-10-16 10:36:43 +0900

    feat: CommunityPostController 생성, 삭제 기능 구현

--- src/main/java/com/sch/rezero/controller/community/CommunityPostController.java
+++ src/main/java/com/sch/rezero/controller/community/CommunityPostController.java
@@ -1,16 +1,25 @@
 package com.sch.rezero.controller.community;
 
+import com.sch.rezero.config.UserContext;
+import com.sch.rezero.dto.community.communityPost.CommunityPostCreateRequest;
 import com.sch.rezero.dto.community.communityPost.CommunityPostQuery;
 import com.sch.rezero.dto.community.communityPost.CommunityPostResponse;
+import com.sch.rezero.dto.community.communityPost.CommunityPostUpdateRequest;
 import com.sch.rezero.dto.response.CursorPageResponse;
 import com.sch.rezero.service.community.CommunityPostService;
+import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommunityPostController {
 
   private final CommunityPostService communityPostService;
+  private final UserContext userContext;
 
   @GetMapping
   public ResponseEntity<CursorPageResponse<CommunityPostResponse>> findAll(CommunityPostQuery query) {
@@ -29,6 +39,28 @@ public class CommunityPostController {
   public ResponseEntity<CommunityPostResponse> findByPost(@PathVariable long postId) {
     communityPostService.findByPostId(postId);
     return ResponseEntity.status(HttpStatus.OK).body(communityPostService.findByPostId(postId));
+  }
+
+  @PostMapping
+  public ResponseEntity<CommunityPostResponse> create(
+      @RequestPart("request") @Valid CommunityPostCreateRequest request,
+      @RequestPart(value = "images", required = false) List<MultipartFile> images
+      ) {
+    Long userId = userContext.getCurrentUserId();
+    CommunityPostResponse created = communityPostService.create(userId, request, images);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(created);
+  }
+
+  @PatchMapping("{postId}")
+  public ResponseEntity<CommunityPostResponse> update(
+      @PathVariable long postId,
+      @RequestPart @Valid CommunityPostUpdateRequest request,
+      @RequestPart(value = "images", required = false) List<MultipartFile> images) {
+    Long userId = userContext.getCurrentUserId();
+
+    CommunityPostResponse updated = communityPostService.update(userId, postId, request);
+    return ResponseEntity.status(HttpStatus.OK).body(updated);
   }
 
 
