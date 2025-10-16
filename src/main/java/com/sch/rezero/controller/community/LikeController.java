18cfd07521418f1dbea54fb19a6953d49edb7a4d
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-10-16 09:40:23 +0900

    style: 안쓰는 import문 삭제

--- src/main/java/com/sch/rezero/controller/community/LikeController.java
+++ src/main/java/com/sch/rezero/controller/community/LikeController.java
@@ -2,8 +2,6 @@ package com.sch.rezero.controller.community;
 
 import com.sch.rezero.config.UserContext;
 import com.sch.rezero.dto.community.like.LikeResponse;
-import com.sch.rezero.entity.community.Like;
-import com.sch.rezero.entity.user.User;
 import com.sch.rezero.mapper.community.LikeMapper;
 import com.sch.rezero.service.community.LikeService;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
