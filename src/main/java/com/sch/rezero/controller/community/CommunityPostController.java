01c2e64574f8109eb4a3e6b6606074f8b547a85c
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-10-16 10:41:03 +0900

    feat: CommunityPostController 수정 기능 구현

--- src/main/java/com/sch/rezero/controller/community/CommunityPostController.java
+++ src/main/java/com/sch/rezero/controller/community/CommunityPostController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -63,6 +64,12 @@ public class CommunityPostController {
     return ResponseEntity.status(HttpStatus.OK).body(updated);
   }
 
+  @DeleteMapping("{postId}")
+  public ResponseEntity<Void> delete(@PathVariable long postId) {
+    Long userId = userContext.getCurrentUserId();
+    communityPostService.delete(userId, postId);
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
 
 
 }
