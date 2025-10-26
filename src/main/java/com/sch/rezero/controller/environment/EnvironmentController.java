3bf35c6d49fff95e01d56206f89e9d84bb43ef1c
Seoyeon-Jeong <seoyeoni945@gmail.com>
2025-10-26 21:56:16 +0900

    fix: 레벨 테스트 DTO 수정

--- src/main/java/com/sch/rezero/controller/environment/EnvironmentController.java
+++ src/main/java/com/sch/rezero/controller/environment/EnvironmentController.java
@@ -1,14 +1,17 @@
 package com.sch.rezero.controller.environment;
 
 import com.sch.rezero.config.UserContext;
+import com.sch.rezero.dto.environment.answer.AnswerResponse;
 import com.sch.rezero.dto.environment.userAnswer.UserAnswerRequest;
 import com.sch.rezero.dto.environment.userAnswer.UserAnswerResponse;
 import com.sch.rezero.entity.user.User;
+import com.sch.rezero.service.environment.AnswerService;
 import com.sch.rezero.service.environment.UserAnswerService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EnvironmentController {
 
   private final UserAnswerService userAnswerService;
+  private final AnswerService answerService;
   private final UserContext userContext;
 
   @PostMapping
@@ -39,8 +43,14 @@ public class EnvironmentController {
   @PostMapping("/redo")
   public ResponseEntity<UserAnswerResponse> update(@RequestBody List<UserAnswerRequest> userAnswerRequests) {
     userContext.getCurrentUser();
-    UserAnswerResponse answer = userAnswerService.create(userContext.getCurrentUserId(), userAnswerRequests);
+    UserAnswerResponse answer = userAnswerService.update(userContext.getCurrentUserId(), userAnswerRequests);
 
     return ResponseEntity.status(HttpStatus.OK).body(answer);
+  }
+
+  @GetMapping("/question")
+  public ResponseEntity<List<AnswerResponse>> findAll() {
+    List<AnswerResponse> answers = answerService.findAll();
+    return ResponseEntity.status(HttpStatus.OK).body(answers);
   }
 }
