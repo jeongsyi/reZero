c47019ea425e44871579779811513c128ea0bb4a
j2eun0922 <j2eun0922@naver.com>
2025-11-03 22:06:23 +0900

    .

--- src/main/java/com/sch/rezero/repository/user/UserRepository.java
+++ src/main/java/com/sch/rezero/repository/user/UserRepository.java
@@ -1,17 +1,18 @@
 package com.sch.rezero.repository.user;
 
-import com.sch.rezero.dto.community.communityPost.CommunityPostResponse;
 import com.sch.rezero.entity.community.CommunityPost;
 import com.sch.rezero.entity.user.User;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> , UserQueryRepository{
+import java.util.Optional;
 
-  Boolean existsByLoginId(String loginId);
-  Optional<User> findByLoginId(String loginId);
-  Optional<CommunityPost> findByName(String name);
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, UserQueryRepository {
+
+    Boolean existsByLoginId(String loginId);
+
+    Optional<User> findByLoginId(String loginId);
+
+    Optional<CommunityPost> findByName(String name);
 }
