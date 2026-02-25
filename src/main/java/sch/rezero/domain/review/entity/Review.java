a928c909b0311004c33d1f938f854ba165185350
Seoyeon-Jeong <seoyeoni945@gmail.com>
2026-02-25 22:31:20 +0900

    feat: Review의 Entity 생성

---
+++ src/main/java/sch/rezero/domain/review/entity/Review.java
@@ -0,0 +1,49 @@
+package sch.rezero.domain.review.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sch.rezero.domain.user.entity.User;
+
+@Entity
+@Getter
+@Table(name = "reviews")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+//  @ManyToOne(fetch = FetchType.LAZY)
+//  @JoinColumn(name = "store_id", nullable = false)
+//  private Store store;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(name = "content", nullable = false)
+  private String content;
+
+  @Column(name = "created_at", nullable = false)
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+  @Column(name = "deleted_at")
+  private LocalDateTime deletedAt;
+}
