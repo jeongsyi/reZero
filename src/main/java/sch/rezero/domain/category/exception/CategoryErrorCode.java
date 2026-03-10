94c0b96827a479b8b5322aa8933a6fc197f1b6f7
j2eun0922 <j2eun0922@naver.com>
2026-03-10 15:38:39 +0900

    feat: 카테고리 커스텀 예외 추가

---
+++ src/main/java/sch/rezero/domain/category/exception/CategoryErrorCode.java
@@ -0,0 +1,18 @@
+package sch.rezero.domain.category.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import sch.rezero.config.exception.ErrorCode;
+
+@Getter
+@AllArgsConstructor
+public enum CategoryErrorCode implements ErrorCode {
+    DUPLICATE_CATEGORY_NAME("C001", "이미 존재하는 카테고리 이름입니다.", HttpStatus.BAD_REQUEST.value()),
+    CATEGORY_NOT_FOUND("C002", "존재하지 않는 카테고리 아이디입니다.", HttpStatus.NOT_FOUND.value())
+    ;
+
+    private final String code;
+    private final String description;
+    private final Integer httpStatus;
+}
